5a5fbace83cd06eaba62287f58e6eab652fc8885
tronghia <ttnghia5112002@gmail.com>
2023-10-17 17:00:17 +0700

    bai4

---
+++ for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // Khởi tạo biến i
+  int i = 1;
+
+  // Vòng lặp for
+  for (i = 1; i <= 10; i++) {
+    // Hiển thị số i
+    printf("%d\n", i);
+  }
+
+  return 0;
+}
